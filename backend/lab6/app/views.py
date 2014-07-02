f56cc7714b709e62d14cdb6b3305d239c60c1910
Alejandro Romero <alejo8591@gmail.com>
2014-07-02 00:32:50 -0500

    Update all Application

--- backend/lab6/app/views.py
+++ backend/lab6/app/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render_to_response, get_object_or_404, HttpResponse
 from django.template import Context, RequestContext
 from app.models import Item, Category, Picture
 from app.forms import CategoryForm, ItemForm
+# Modulo serializador de QuerySets para Django
+from django.http import HttpResponse
+from django.core.serializers import serialize
+import json
 
 def index(request):
 	context = Context({'title' : 'Hola CIDEI'})
@@ -32,7 +36,7 @@ def add_category(request):
 	else:
 		form = CategoryForm()
 
-	context = Context({'title':'Creación de categorias', 'form': form, 'info_button' : 'Crear Categoria'})
+	context = Context({'title':'Creación de categorias', 'form': form})
 	return render_to_response('add-category.html', context, context_instance=RequestContext(request))
 
 def edit_category(request, slug):
@@ -93,3 +97,59 @@ def add_item(request):
 
 	context = Context({'title' : 'Adicionar item', 'form' : form})
 	return render_to_response('add-item.html', context, context_instance=RequestContext(request))
+
+def items(request):
+	items = Item.objects.all()
+	context = Context({'title' : 'Hola CIDEI', 'items' : items})
+	return render_to_response('items.html', context)
+
+def item(request, item_id):
+	item = get_object_or_404(Item, id=item_id)
+	pictures = Picture.objects.filter(item=item)
+	count_pictures = pictures.count()
+	context = Context({
+		'title' : 'Hola CIDEI',
+		'item' : item,
+		'pictures':pictures,
+		'count_pictures' : count_pictures
+	})
+	return render_to_response('item-details.html', context)
+
+def add_item(request):
+	if request.method == "POST":
+		form = ItemForm(request.POST)
+		if form.is_valid():
+			# Crear un nuevo item
+			item = Item.objects.create(
+				listing = form.cleaned_data['listing'],
+				name = form.cleaned_data['name'],
+				category=form.cleaned_data['category'],
+				department=form.cleaned_data['department'],
+				description = form.cleaned_data['description'],
+				update_item = form.cleaned_data['update_item'],
+			)
+			# Siempre que cree el dato correctamente redireccionar
+			return HttpResponseRedirect('/items/%s/' % item.id)
+	else:
+		form = ItemForm()
+
+	context = Context({'title' : 'Adicionar item', 'form' : form})
+	return render_to_response('add-item.html', context, context_instance=RequestContext(request))
+
+def ajax_items(request):
+	if request.is_ajax():
+		items = Item.objects.all()
+		print items
+		items = serialize('json', items)
+		print items
+		items = json.dumps(items)
+		print items
+
+		return HttpResponse(items, content_type='application/json')
+	else:
+		return HttpResponse({'error' : 'Hubo un error'}, content_type='application/json')
+
+
+
+
+
