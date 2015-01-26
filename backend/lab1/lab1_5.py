e49a04c49110563b2ccfe8fd4a278b07478a6714
Alejandro Romero <alejo8591@gmail.com>
2015-01-26 08:44:18 -0500

    Change "raw_input" for "input"

--- backend/lab1/lab1_5.py
+++ backend/lab1/lab1_5.py
@@ -1,10 +1,10 @@
 sum = 0.0 
-data = raw_input("Ingrese un numero: ")
+data = input("Ingrese un numero: ")
 
 while data != "":
 	number = float(data)
 	sum+=number
-	data = raw_input("Ingrese el siguiente numero: ")
+	data = input("Ingrese el siguiente numero: ")
 	number = float(data)
 	sum+=number
 
