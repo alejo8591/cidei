cce5eec8aa6e82d4bbfedb4f5a2dc5c58f7d4c90
Alejandro Romero <alejo8591@gmail.com>
2015-01-26 10:53:54 -0500

    Update lab1_6.py

--- backend/lab1/lab1_6.py
+++ backend/lab1/lab1_6.py
@@ -1,22 +1,22 @@
 # Importante modulo completo "random" en python
 import random
 
-smaller = int(input("Ingrese el numero pequeno: "))
-large =  int(input("Ingrese el numero grande o mayor: ")) 
+smaller = int(input("Ingrese el número pequeno: "))
+large =  int(input("Ingrese el número grande o mayor: ")) 
 
 my_random_number = random.randint(smaller, large)
 
-print("My numero aleatorio es: ", my_random_number)
+print("My número aleatorio es: ", my_random_number)
 
 count = 0
 
 while True:
 	count+=1
-	user_number = input("Ingrese un numero para comparar: ")
+	user_number = int(input("Ingrese un número para comparar: "))
 	if user_number < my_random_number:
-		print("Es un numero pequeno")
+		print("Es un número pequeno")
 	elif user_number > my_random_number:
-		print("Es un numero grande")
+		print("Es un número grande")
 	else:
 		print("Lo tienes en ", count, "Vuelve a intentarlo!")
 		break
