7b9b1fc744760f1ac2e813b4df25fd2d275ae557
Alejandro Romero <alejo8591@gmail.com>
2015-01-26 08:50:41 -0500

    Update lab1_8.py

--- backend/lab1/lab1_8.py
+++ backend/lab1/lab1_8.py
@@ -7,7 +7,7 @@ Convertir de decimal a binario
 decimal = input("Ingrese el número decimal a Convertir: ")
 
 if decimal == 0:
-	print 0
+	print(0)
 else:
 	print "Resto del cociente binario"
 	bstring = ""
@@ -16,4 +16,4 @@ else:
 		decimal = decimal / 2
 		bstring = str(remainder) + bstring
 		print "%5d%8d%12s" % (decimal, remainder, bstring)
-	print "La representación binaria es: ", bstring
+	print("La representación binaria es: ", bstring)
